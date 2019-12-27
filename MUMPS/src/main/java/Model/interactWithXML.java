f36b107bab96e41c80a3baa3fca0cfc7a866948d
Squidgeypea <sophie.turner@students.plymouth.ac.uk>
2019-12-27 21:08:59 +0000

    Expanded XML to include appointment and feedback data. Next is to include medicines.

--- MUMPS/src/main/java/Model/interactWithXML.java
+++ MUMPS/src/main/java/Model/interactWithXML.java
@@ -14,13 +14,37 @@ import javax.xml.parsers.*; //store XML elements as DOM objects.
 public class interactWithXML {
     
     public void interact(){
-        Document userFile = getDocument("./src/main/java/Model/userFile.xml");
+        Document userFile = getDocument("./src/main/java/dataFiles/userFile.xml");
         System.out.println("The root of the XML file is: " + 
                 userFile.getDocumentElement().getNodeName());
         NodeList listOfDoctors = userFile.getElementsByTagName("doctor"); 
         System.out.println("Number of doctors: " + 
                 listOfDoctors.getLength());
-                //Some tests.
+                //Some tests
+        String doctorForename = "forename";
+        String doctorSurname = "surname";
+        getDoctorNames(listOfDoctors, doctorForename, doctorSurname);
+    }
+    
+    public void getDoctorNames(NodeList listOfDoctors, String doctorForename, String doctorSurname){
+        //This method shows the forenames of each doctor.
+        for(int i = 0; i<listOfDoctors.getLength(); i++){
+            Node doctorNode = listOfDoctors.item(i);
+            Element doctorElement = (Element)doctorNode;
+                 //Turn it into an element                    
+            NodeList fornameList = doctorElement.getElementsByTagName(doctorForename);
+            NodeList surnameList = doctorElement.getElementsByTagName(doctorSurname);            
+                 //Create list of elements
+            Element fornameElement = (Element)fornameList.item(0);
+            Element surnameElement = (Element)surnameList.item(0);
+                //get first element
+            NodeList forenameElementList = fornameElement.getChildNodes();
+            NodeList surnameElementList = surnameElement.getChildNodes();
+                //get contents of forename field
+            System.out.println(
+                    ((Node)forenameElementList.item(0)).getNodeValue().trim() + " " +
+                    ((Node)surnameElementList.item(0)).getNodeValue().trim() );
+        }
     }
     
     public static Document getDocument(String fileSource){
