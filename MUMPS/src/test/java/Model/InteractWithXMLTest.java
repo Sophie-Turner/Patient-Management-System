945f3327c270bf4b2999a360dd0bb5f32dc83cf0
Squidgeypea <sophie.turner@students.plymouth.ac.uk>
2020-01-12 14:53:22 +0000

    Made some JUnit tests.

--- MUMPS/src/test/java/Model/InteractWithXMLTest.java
+++ MUMPS/src/test/java/Model/InteractWithXMLTest.java
@@ -11,12 +11,16 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
+import Model.InteractWithXML;
 
 /**
  *
  * @author pipin
  */
 public class InteractWithXMLTest {
+    private String filesource;
     
     public InteractWithXMLTest() {
     }
@@ -31,6 +35,7 @@ public class InteractWithXMLTest {
     
     @Before
     public void setUp() {
+        filesource = "./src/main/java/dataFiles/userFile.xml";
     }
     
     @After
@@ -39,10 +44,32 @@ public class InteractWithXMLTest {
 
     @Test
     public void testGetDocument() {
+        System.out.println("Testing getDocument");
+        assertFalse("See if the file failed to be parsed and returned. Hopefully this will be false.", 
+                InteractWithXML.getDocument(filesource) == null);
+        
     }
 
     @Test
     public void testGetAllPatientNames() {
+        String patientName;
+        String expectedName = "Mona Lotte";
+        System.out.println("Testing retrieving one patient's name.");
+        Document file=InteractWithXML.getDocument(filesource);
+        NodeList listOfPatients = file.getElementsByTagName("patient");
+        Node patientNode = listOfPatients.item(0);
+        Element patientElement = (Element)patientNode;
+        NodeList nameList = patientElement.getElementsByTagName("name");
+        Element nameElement = (Element)nameList.item(0);
+        NodeList nameElementList = nameElement.getChildNodes();
+        patientName = ((Node)nameElementList.item(0)).getNodeValue();
+        assertEquals("Name should be Mona Lotte", expectedName, patientName);
+        
+        System.out.println("Testing getAllPatientNames");
+        int expectedNumber = 4;
+        String[] patientList = InteractWithXML.getAllPatientNames();
+        int countedList = patientList.length;
+        assertEquals("patientList should contain 4 different names.", expectedNumber, countedList);
     }
 
     @Test
